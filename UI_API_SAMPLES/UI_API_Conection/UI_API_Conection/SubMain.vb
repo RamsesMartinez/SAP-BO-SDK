c3c87ed000742842ed0c3c3148da43cb196d43fa
Ramses Martinez <ramses.mtz96@gmail.com>
2017-10-06 13:31:47 -0500

    Add basic ui api connection sample

---
+++ UI_API_SAMPLES/UI_API_Conection/UI_API_Conection/SubMain.vb
@@ -0,0 +1,12 @@
+﻿Option Strict Off
+Option Explicit On
+Module SubMain
+
+    Public Sub Main()
+
+        Dim oHelloWorld As HelloWorld
+
+        oHelloWorld = New HelloWorld
+
+    End Sub
+End Module
